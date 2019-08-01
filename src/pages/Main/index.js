4f9b5726eb2c54729f5f88a6975e01bca0b5c684
Denilson da Silva Rocha <ddrocha@server01.foursys.local>
2019-08-01 18:26:14 -0300

    projeto inicial

---
+++ src/pages/Main/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Form, Input } from 'react-native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import { Container, Form, Input, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeHolder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
+}
+
+Main.navigationOptions = {
+  title: 'Usuários',
+};
