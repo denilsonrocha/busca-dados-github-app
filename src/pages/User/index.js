8d6df5cff859b695cae63a388ebd0eb16678c114
Denilson da Silva Rocha <ddrocha@server01.foursys.local>
2019-08-05 17:51:08 -0300

    melhorias no codigo

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Componet } from 'react';
 import { View } from 'react-native';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+class User extends Componet {
+  static navigationOptions = ({ navigation }) => ({
+    title: navigation.getParam('user').name,
+  });
 
-export default function User() {
-  return <View />;
+  state = {
+    stars: [],
+  };
+
+  async componentDidMount() {
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    const response = await api.get(`/users/${user.login}/starred`);
+
+    this.setState({ stars: response.data });
+  }
+
+  render() {
+    return <View />;
+  }
 }
+
+export default User;
